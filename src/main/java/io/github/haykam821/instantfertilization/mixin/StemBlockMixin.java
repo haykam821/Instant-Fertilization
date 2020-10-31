d0bba2460243a415c4921eda75557cee5b1a6dd7
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-10-31 12:26:58 -0400

    Fix pumpkin and melon stems not always growing to their last stage

--- src/main/java/io/github/haykam821/instantfertilization/mixin/StemBlockMixin.java
+++ src/main/java/io/github/haykam821/instantfertilization/mixin/StemBlockMixin.java
@@ -8,17 +8,20 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.haykam821.instantfertilization.Main;
-import net.minecraft.block.AbstractPlantStemBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.StemBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 
-@Mixin(AbstractPlantStemBlock.class)
-public class AbstractPlantStemBlockMixin {
+@Mixin(StemBlock.class)
+public abstract class StemBlockMixin {
 	@Inject(method = "grow", at = @At("HEAD"), cancellable = true)
 	private void growToMaximum(ServerWorld world, Random random, BlockPos pos, BlockState state, CallbackInfo ci) {
 		if (!state.isIn(Main.INSTANT_FERTILIZATION_BLACKLIST)) {
-			world.setBlockState(pos, state.with(AbstractPlantStemBlock.AGE, 25));
+			BlockState newState = state.with(StemBlock.AGE, 7);
+			world.setBlockState(pos, newState);
+			newState.randomTick(world, pos, random);
+
 			ci.cancel();
 		}
 	}
